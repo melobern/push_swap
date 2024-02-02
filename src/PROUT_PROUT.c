27aae743414ffb7efa98cfc6ca9ec7841845db4e
mbernard <melodie.bernard123@yahoo.fr>
2024-02-02 16:18:05 +0100

    why WHY

--- src/PROUT_PROUT.c
+++ src/PROUT_PROUT.c
@@ -6,7 +6,7 @@
 /*   By: mbernard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 11:20:18 by mbernard          #+#    #+#             */
-/*   Updated: 2024/01/30 18:39:52 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/02/02 15:33:51 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,12 @@ void	show_pile(t_nodes_list **pile)
 	}
 	tmp = *pile;
 	printf("%i\t", tmp->value);
-	printf("TOPTHREE === %i\n", tmp->top_three);
+	//printf("TOPTHREE === %i\n", tmp->top_three);
 	while (tmp->next)
 	{
 		tmp = tmp->next;
 		printf("%i\t", tmp->value);
-		printf("TOPTHREE === %i\n", tmp->top_three);
+	//	printf("TOPTHREE === %i\n", tmp->top_three);
 	}
+	printf("\n");
 }
