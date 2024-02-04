3f774564857a53c5b9b4f63ce599cc890064708f
melodie <melodie.bernard123@yahoo.fr>
2024-02-04 18:41:28 +0100

    sort but is LENT

--- src/sort/push_into_b.c
+++ src/sort/push_into_b.c
@@ -6,7 +6,7 @@
 /*   By: mbernard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 12:01:31 by mbernard          #+#    #+#             */
-/*   Updated: 2024/02/03 14:37:20 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/02/04 17:33:03 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,12 @@ static size_t	pos_node(int num, t_nodes_list **pile_b)
 			position++;
 		b = b->next;
 	}
-	printf("Position de %i est %li\n", num, position);
+//	printf("Position de %i est %li\n", num, position);
 	return (position);
 }
 
-static void	move_position(t_nodes_list **pile_a, t_nodes_list **pile_b, size_t len)
+static void	move_position(t_nodes_list **pile_a, t_nodes_list **pile_b,
+		size_t len)
 {
 	size_t	x;
 	size_t	position;
@@ -47,7 +48,7 @@ static void	move_position(t_nodes_list **pile_a, t_nodes_list **pile_b, size_t l
 	p_len = len;
 	while (position > 0 && x > 0 && (len > 1 && p_len >= len))
 	{
-	show_pile(pile_b);           ////
+	//	show_pile(pile_b); ////
 		if (position < (len - position))
 			ft_putendl_fd(rb(pile_b), 1);
 		else
@@ -55,23 +56,21 @@ static void	move_position(t_nodes_list **pile_a, t_nodes_list **pile_b, size_t l
 			len = len - position;
 			while (len--)
 				ft_putendl_fd(rrb(pile_b), 1);
-			ft_putendl_fd("prout", 1);
 		}
 		x--;
 	}
 	ft_putendl_fd(pb(pile_b, pile_a), 1);
-			ft_putendl_fd("prout prout", 1);
-	show_pile(pile_b);           ////
+//	show_pile(pile_b); ////
 }
 
 static void	chose_move(t_nodes_list **pile_a, t_nodes_list **pile_b)
 {
 	size_t	b_len;
-	int	a;
+	int		a;
 
 	a = (*pile_a)->value;
 	b_len = pile_len(pile_b);
-	if (b_len < 2 || (pile_rev_sorted(pile_b) && (is_min(a, pile_b) || is_max(a, pile_b))))
+	if (b_len < 2 || (pile_rev_sorted(pile_b) && (is_min_max(a, pile_b))))
 	{
 		ft_putendl_fd(pb(pile_b, pile_a), 1);
 		if (b_len > 0 && is_min((*pile_b)->value, pile_b))
@@ -93,12 +92,25 @@ void	push_into_a(t_nodes_list **pile_a, t_nodes_list **pile_b)
 
 void	push_into_b(t_nodes_list **pile_a, t_nodes_list **pile_b)
 {
+	size_t	pos_a;
+	size_t	pos_n;
+	int	actual_n;
+	int	next_n;
+
 	if (!(*pile_a))
 		return ;
-	while ((*pile_a)->next->next->next != NULL)
+	while ((*pile_a)->next->next->next != NULL && !pos_linear(pile_a))
 	{
-		if (is_top_three(*pile_a))
+		pos_a = (*pile_a)->pos;
+		pos_n = (*pile_a)->next->pos;
+		actual_n = (*pile_a)->value;
+		next_n = (*pile_a)->next->value;
+		if (is_top_three(*pile_a) && pos_a == pos_n + 1)
+			ft_putendl_fd(sa(pile_a), 1);
+		else if (is_top_three(*pile_a))
 			ft_putendl_fd(ra(pile_a), 1);
+		else if (!is_min_max(next_n, pile_b) && is_min_max(actual_n, pile_b))
+			ft_putendl_fd(sa(pile_a), 1);
 		else
 			chose_move(pile_a, pile_b);
 		if (pos_linear(pile_a))
